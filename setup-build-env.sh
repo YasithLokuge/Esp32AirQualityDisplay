3b33470c215938386ad68b017e10faf5df8fb0bf
yasithlokuge <yasith1@gmail.com>
2020-07-21 16:38:40 +1000

    sudo

--- setup-build-env.sh
+++ setup-build-env.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # copied from https://codeblog.dotsandbrackets.com/gitlab-ci-esp32-arduino/
-apt-get update
+sudo apt-get update
 cd ~
 
 # Install arduino-cli
-apt-get install curl -y
+sudo apt-get install curl -y
 curl -L -o arduino-cli.tar.bz2 https://downloads.arduino.cc/arduino-cli/arduino-cli-latest-linux64.tar.bz2
 tar xjf arduino-cli.tar.bz2
 rm arduino-cli.tar.bz2
 mv `ls -1` /usr/bin/arduino-cli
 
 # Install python, pip and pyserial
-apt-get install python -y
+sudo apt-get install python -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install pyserial
@@ -30,7 +30,7 @@ arduino-cli lib install "MPU9250_asukiaaa"
 cd -
 
 # Install 'third-party' packages: find proper location and 'git clone'
-apt-get install git -y
+sudo apt-get install git -y
 cd `arduino-cli config dump | grep sketchbook | sed 's/.*\ //'`/libraries
 git clone https://github.com/me-no-dev/AsyncTCP.git
 git clone https://github.com/me-no-dev/ESPAsyncWebServer.git
